ae3005e18edbb4202a4b7ec967646b2852140e65
gabezolla <gabriel.zolla1998@gmail.com>
2023-03-20 00:06:16 -0300

    add new images to test face_recognition

--- face-recognition-lib/main.py
+++ face-recognition-lib/main.py
@@ -12,7 +12,7 @@ known_faces = [obama_face_encoding]
 # Initialize an empty array to store the images
 images = []
 encodings = []
-numberOfObamas = 0
+countedObamas = 0
 
 # Loop over the files in the directory and load each image
 for file in os.listdir(image_dir):
@@ -26,6 +26,6 @@ for file in os.listdir(image_dir):
         print(f"This image is {file}")
         print("Is this image Obama? {}".format(face_recognition.compare_faces(known_faces, encoding)))
         if(face_recognition.compare_faces(known_faces, encoding)[0] == True):
-            numberOfObamas += 1 
+            countedObamas += 1 
 
-print(f"Obamas counted: {numberOfObamas}")
+print(f"Obamas counted: {countedObamas}")
